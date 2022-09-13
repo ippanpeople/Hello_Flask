8b6ad9744bf2e39d5f3a9997f32cb0e92d1dca5d
rinriN <2210368@ecc.ac.jp>
2022-09-13 14:59:42 +0900

    update : directions about Decorator which @app

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 
 app = Flask(__name__) #__name__是python內建的一個變數, 會儲存目前這個程式在哪個模組下進行執行
 		      #如果這個程式是被當成主程式被執行的話__name__ = __main__
-@app.route("/")	#函式的裝飾
+
+@app.route("/")	#函式的裝飾(Decorator):以韓式為基礎, 提供附加的功能
 def home():
 	return "Hello Flask"
 
